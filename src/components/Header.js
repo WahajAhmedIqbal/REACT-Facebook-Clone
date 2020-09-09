594a0c3e9fc94f15cb56814ee5b8bdccbc1bda45
wahaj ahmed iqbal <wahaj465@gmail.com>
2020-09-09 21:02:33 +0500

    icon 45

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,17 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import FlagIcon from "@material-ui/icons/Flag";
+import SubscriptionsOutlinedIcon from "@material-ui/icons/SubscriptionsOutlined";
+import StorefrontOutlinedIcon from "@material-ui/icons/StorefrontOutlined";
+import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle";
+import { Avatar, IconButton } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import ForumIcon from "@material-ui/icons/Forum";
+import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
 function Header() {
   return (
     <div className="header">
@@ -8,9 +20,48 @@ function Header() {
           alt="logo"
           src="http://www.pngplay.com/wp-content/uploads/3/Facebook-Logo-PNG-Clipart-Background.png"
         />
+        <div className="header_input">
+          <SearchIcon />
+          <input type="text"></input>
+        </div>
+        {/*---------------------- HEADER CENTER PART ---------------------*/}
       </div>
-      <div className="header_middle"></div>
-      <div className="header_right"></div>
+      <div className="header_middle">
+        <div className="header_option">
+          <HomeIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <FlagIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <SubscriptionsOutlinedIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <StorefrontOutlinedIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <SupervisedUserCircleIcon fontSize="large" />
+        </div>
+      </div>
+      {/* ------------------------- RIGHT PART HEADER */}
+      <div className="header_right">
+        <div className="header_info">
+          <Avatar />
+          <h4>Wahaj Ahmed</h4>
+        </div>
+        <IconButton>
+          <AddIcon />
+        </IconButton>
+        <IconButton>
+          <ForumIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsActiveIcon />
+        </IconButton>
+        <IconButton>
+          <ExpandMoreIcon />
+        </IconButton>
+      </div>
     </div>
   );
 }
