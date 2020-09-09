3463e6a6742694891ebf6b044ec9ec949a3ab9ac
wahaj ahmed iqbal <wahaj465@gmail.com>
2020-09-09 19:44:44 +0500

    setup

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css";
+function Header() {
+  return (
+    <div className="header">
+      <div className="header_left">
+        <img
+          alt="logo"
+          src="http://www.pngplay.com/wp-content/uploads/3/Facebook-Logo-PNG-Clipart-Background.png"
+        />
+      </div>
+      <div className="header_middle"></div>
+      <div className="header_right"></div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAcvsOVBaA40ahLrckpTm8cj6GBWKu715s",
+  authDomain: "facebook-clone-ef3e1.firebaseapp.com",
+  databaseURL: "https://facebook-clone-ef3e1.firebaseio.com",
+  projectId: "facebook-clone-ef3e1",
+  storageBucket: "facebook-clone-ef3e1.appspot.com",
+  messagingSenderId: "356138527355",
+  appId: "1:356138527355:web:c9bee0222c4420795590de",
+  measurementId: "G-Y8QH92MX8M",
+};
